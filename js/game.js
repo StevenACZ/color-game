f448bd31804481ffa0c160ecbbdbc87088870aad
stevenacz <scoailazaa@gmail.com>
2021-01-21 13:48:15 -0500

    Header y navbar estilos terminados

--- js/game.js
+++ js/game.js
@@ -14,8 +14,60 @@ const randomColor = () => {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
+let btnSelected = {
+  btnEasy: false,
+  btnHard: false
+} 
+
+// Before all
+let rand = randomColor();
+header.style.backgroundColor = rand;
+title.textContent = rand;
+btnNewColors.style.color = rand;
+btnEasy.style.color = rand;
+btnHard.style.color = rand;
+
 btnNewColors.addEventListener('click', (event) => {
-  const rand = randomColor();
+  rand = randomColor();
+  
   header.style.backgroundColor = rand;
   title.textContent = rand;
+  btnNewColors.style.color = rand;
+  btnEasy.style.color = rand;
+  btnHard.style.color = rand;
+  title.textContent = rand;
+
+  if (btnSelected.btnEasy) {
+    btnEasy.style.backgroundColor = rand;
+    btnEasy.style.color = 'white'
+  } else if (btnSelected.btnHard) {
+    btnHard.style.backgroundColor = rand;
+    btnHard.style.color = 'white'
+  }
 });
+
+btnEasy.addEventListener('click', (event) => {
+  btnSelected = {
+    btnEasy: true,
+    btnHard: false
+  }
+
+  event.target.style.backgroundColor = rand;
+  event.target.style.color = 'white';
+
+  btnHard.style.backgroundColor = 'transparent';
+  btnHard.style.color = rand;
+})
+
+btnHard.addEventListener('click', (event) => {
+  btnSelected = {
+    btnEasy: false,
+    btnHard: true
+  } 
+
+  event.target.style.backgroundColor = rand;
+  event.target.style.color = 'white';
+
+  btnEasy.style.backgroundColor = 'transparent';
+  btnEasy.style.color = rand;
+})
